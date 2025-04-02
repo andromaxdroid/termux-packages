05745b2e7d97d36b18b50e2e0944f9dc36a79811
andromaxdroid <andromax210894@gmail.com>
2025-04-02 23:49:01 +0800

    Create build.sh

---
+++ packages/whisper/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/andromaxdroid
+TERMUX_PKG_DESCRIPTION="whisper.cpp & autotrancibe"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@andromaxdroid"
+TERMUX_PKG_VERSION=1.0.0
+TERMUX_PKG_SRCURL=https://github.com/andromaxdroid/yourtool/archive/refs/tags/v1.0.0.tar.gz
+TERMUX_PKG_SHA256=<SHA256_CHECKSUM>
+TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_BUILD_IN_SRC=true
