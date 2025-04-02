5bc81d46b9620a3ee3c8db0c88fd859cf100fefb
andromaxdroid <andromax210894@gmail.com>
2025-04-02 23:59:07 +0800

    Update build.sh

--- packages/whisper/build.sh
+++ packages/whisper/build.sh
@@ -3,7 +3,13 @@ TERMUX_PKG_DESCRIPTION="whisper.cpp & autotrancibe"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@andromaxdroid"
 TERMUX_PKG_VERSION=1.0.0
-TERMUX_PKG_SRCURL=https://github.com/andromaxdroid/yourtool/archive/refs/tags/v1.0.0.tar.gz
+TERMUX_PKG_SRCURL=https://github.com/andromaxdroid/whisper.cpp/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=<SHA256_CHECKSUM>
-TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_DEPENDS="cmake, ffmpeg"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+
+termux_step_make() {
+	
+	make
+}
